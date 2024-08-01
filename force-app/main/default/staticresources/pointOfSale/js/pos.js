c5ef6985c5dd723f676a42aecb06ee32b881344a
Mona Lisa <MY_NAME@example.com>
2024-08-01 16:45:03 +0530

    search products

--- force-app/main/default/staticresources/pointOfSale/js/pos.js
+++ force-app/main/default/staticresources/pointOfSale/js/pos.js
@@ -4,10 +4,12 @@ function posPageLoaded() {
     console.table(productwrap);
     var posProductPageListenerAdded = false;
     var posPageWrapperContainer = document.querySelector('.product-cart-wrap');
+    
     if ((posPageWrapperContainer) && (!posProductPageListenerAdded)) {
         posProductPageListenerAdded = true;
         // Click handler for entire DIV posPageWrapperContainer
         posPageWrapperContainer.addEventListener('click', function (e) {
+            console.log('methodcalled product-wrapper');
             // Click handler for entire DIV product-wrapper
             if ((e.target.classList.contains('product-wrapper'))) {
                 console.log('methodcalled product-wrapper');
@@ -16,10 +18,17 @@ function posPageLoaded() {
                 console.log('selectedProductIdNDR ' + selectedProductId);
                 addtocart(selectedProductId);
                 gettingorderDetails();
-            }
+           }
         });
     }
 
     //Chain Another Action functions 
     gettingorderDetails();
 }
+function productFiltering() {
+    var product = document.getElementById("productname");
+    console.log('x-value' + product.value);
+    var productname = product.value;
+    ProductFiltering(productname);
+    
+  }
